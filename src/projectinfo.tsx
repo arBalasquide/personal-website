dfa8b57196c1f4279ed05eec74619533f6999172
arBalasquide <adrian.balasquide@gmail.com>
2021-01-17 15:35:24 -0400

    Completed translations

--- src/projectinfo.tsx
+++ src/projectinfo.tsx
@@ -9,35 +9,35 @@ export const cards = {
     website: {
         title: "Websites",
         EN: "Host various personal websites, such as this one.",
-        日本語: "",
+        日本語: "サイトのサーバを動かすことです。",
         dependencies: "React / Typescript / Node",
         url: "https://github.com/arBalasquide/personal-website"
     },
     amazon_bot: {
         title: "Amazon Auto Buy Bot",
         EN: "Python Bot that buys items for you in US Amazon stores.",
-        日本語: "",
+        日本語: "Amazonで商品を自動的に買うボットです。",
         dependencies: "Python / Selenium",
         url: "https://github.com/arBalasquide/amazon-automation"
     },
     devops: {
         title: "Linux Hosts",
         EN: "Mantain services such as an email server and Jitsi instance.",
-        日本語: "",
+        日本語: "楽しいから、メールとJitsiとかサーバを動かす。",
         dependencies: "NGINX / POSTFIX / Bash",
         url: "",
     },
     linux_scripts: {
         title: "Linux Scripts",
         EN: "Collection of scripts I use daily and for teaching others about Linux.",
-        日本語: "やっべだ",
+        日本語: "日常でよく使うスクリプト。",
         dependencies: "Bash / Python",
         url: "https://github.com/arBalasquide/linux-scripts",
     },
     aoc: {
         title: "AoC2020",
         EN: "Solutions to coding problems for the Advent of Code yearly contests.",
-        日本語: "ASD",
+        日本語: "競争プログラミングの質問の解答。",
         dependencies: "Python",
         url: "https://github.com/arBalasquide/aoc2020",
     }
