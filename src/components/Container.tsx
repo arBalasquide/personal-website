39409fc474d68fcf6cf63bc116710025f8e00f40
arBalasquide <adrian.balasquide@gmail.com>
2021-02-03 23:14:28 -0400

    Fixed responsiveness in smaller screens

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,21 +1,17 @@
-import { Flex, useColorMode } from '@chakra-ui/react'
+import { Flex, useColorMode } from "@chakra-ui/react";
 
 export const Container = (props) => {
-  const { colorMode } = useColorMode()
+  const { colorMode } = useColorMode();
 
-  const bgColor = { light: 'gray.50', dark: 'gray.900' }
-
-  const color = { light: 'black', dark: 'white' }
+  const color = { light: "black", dark: "white" };
   return (
     <Flex
-      height={{sm: "20em", md: "40em", lg:"60em", xl: "160em"}}
       direction="column"
       alignItems="center"
       justifyContent="flex-start"
-      bg={bgColor[colorMode]}
       color={color[colorMode]}
       width="100%"
       {...props}
     />
-  )
-}
+  );
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,20 @@
-import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
-import { useState } from 'react'
+import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
 
-import theme from '../theme'
+import theme from "../theme";
 
 function MyApp({ Component, pageProps }) {
-  const [language, setLanguage] = useState("EN");
-
   return (
     <ChakraProvider resetCSS theme={theme}>
       <ColorModeProvider
         options={{
-          useSystemColorMode: true,
+          useSystemColorMode: false,
+          initialColorMode: "dark",
         }}
       >
         <Component {...pageProps} />
       </ColorModeProvider>
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
